216f7c0a404277cc908f381c066c2c511d380bc3
Md Noorullah Raza <mnraza94@gmail.com>
2025-11-03 00:43:56 +0530

    understand Optional, List and Dict from typing module

--- basics/main.py
+++ basics/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from .user import User
 
 app = FastAPI()
 
 @app.get("/")
-def hello():
-    return {"message": "Welcome to FastAPI 🚀"}
+def hello(person:User):
+    return person
+
+person = User(name="John Doe", age=30, email="john.doe@gmail.com", skills=["Python", "FastAPI"], contact_details={"phone": "123-456-7890"})
 
--- basics/user.py
+++ basics/user.py
@@ -1,7 +1,11 @@
 from pydentic import BaseModel
+from typing import Optional, List, Dict
 
 class User(BaseModel):
     name: str
     age: int
     email: str
-    
+    married: bool = False
+    skills: List[str]
+    address: Optional[str] = None
+    contact_details: Dict[str, str] 
