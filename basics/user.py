f5ddd3f5e6cc8c78c4de0be10f81576270a1db3b
Md Noorullah Raza <mnraza94@gmail.com>
2025-11-03 00:49:46 +0530

    refactor User model to enforce validation on name and age fields

--- basics/user.py
+++ basics/user.py
@@ -2,11 +2,11 @@ from pydentic import BaseModel,EmailStr, AnyUrl
 from typing import Optional, List, Dict
 
 class User(BaseModel):
-    name: str
-    age: int
+    name: str = Field(..., min_length=2, max_length=50)
+    age: int = Field(..., gt=0, description="Age must be a positive integer")
     portfolio: Optional[AnyUrl] = None
     email: EmailStr
     married: bool = False
-    skills: List[str]
+    skills: List[str] = Field(max_length=5)
     address: Optional[str] = None
     contact_details: Dict[str, str] 
