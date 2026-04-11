d5e632b64ee1770d243dfe4eb1785f01ae92f03f
Noorullah <mnraza.dev@gmail.com>
2026-04-11 17:59:39 +0530

    add delete request

--- basics/src/main.py
+++ basics/src/main.py
@@ -3,9 +3,13 @@ from . mockData import products
 from .schemas import Product
 app = FastAPI()
 
+# GET - List all Product 
+
 @app.get("/products")
 def get_all_products():
     return products
+
+# GET - Single Product 
 
 @app.get("/products/{product_id}")
 def get_single_product(product_id: int):
@@ -16,7 +20,7 @@ def get_single_product(product_id: int):
         "error":"Product not found"
     }
     
-# Create a Product 
+# POST - Create a Product 
 
 @app.post("/products/create")
 def create_product(product: Product):
@@ -39,3 +43,18 @@ def update_product(product_id: int, product:Product):
                 "data":p
             }
     return {"error": "Product not found"}
+
+# DELETE - DELETE PRODUCT
+
+@app.delete("/products/delete/{product_id}")
+def delete_product(product_id:int):
+    for index, p in enumerate(products):
+        if p.get('id')  ==  product_id:
+           deleted_product = products.pop(index)
+           return {
+               "msg":"Product deleted successfully!",
+               "data": deleted_product
+           }
+    return {
+        "err":"Product not found"
+    }
