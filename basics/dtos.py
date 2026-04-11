285f2bfff58bee9fae8629668df867cb52a13da7
Noorullah <mnraza.dev@gmail.com>
2026-04-11 16:56:24 +0530

    add CRUD

--- basics/dtos.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class ProductDTO(BaseModel):
-    id:int 
-    title:str
-    price:int = 0
-    quantity:int = 0
---
+++ basics/src/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Request
+from . mockData import products
+from .schemas import Product
+app = FastAPI()
+
+@app.get("/products")
+def get_all_products():
+    return products
+
+@app.get("/products/{product_id}")
+def get_single_product(product_id: int):
+    for singleproduct in products:
+        if singleproduct.get("id") == product_id:
+            return singleproduct
+    return {
+        "error":"Product not found"
+    }
+    
+# Create a Product 
+
+@app.post("/products/create")
+def create_product(product: Product):
+    new_product = product.model_dump()
+    products.append(new_product)
+    return{
+        "msg":"Product added successfully!",
+        "data":products
+    }
+
+# PUT - UPDATE PRODUCT
+
+@app.put("/products/update/{product_id}")
+def update_product(product_id: int, product:Product):
+    for p in products :
+        if p.get('id') == product_id:
+            p.update(product.model_dump())
+            return {
+                "msg":"Product updated successfully!",
+                "data":p
+            }
+    return {"error": "Product not found"}
